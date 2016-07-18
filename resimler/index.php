cb82c189ece4debee07d569e72cfdcd9b71bbac5
Caner Cantürk <canercanturk01@hotmail.com>
2016-07-18 14:43:40 +0300

    s

---
+++ resimler/index.php
@@ -0,0 +1,20 @@
+<?php
+    $resimler = array('kapadokya-1.jpg','kapadokya-2.jpg','kapadokya-3.jpg','kapadokya-4.jpg');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>RESİMLER</h1>
+    <hr color="blue">
+    <h2><a href="../index.html"> Geri dön</a></h2><br>
+        <?php for($i=0; $i< count($resimler); $i++){ ?>
+          <a href="goster.php?dosya=<?php echo $resimler[$i]; ?>"><img height="80px" src="<?php echo $resimler[$i]; ?>" alt="Kapadokya manzaraları"/></a>
+        <?php } ?>
+      </tr>
+     </table>
+  </body>
+</html>
